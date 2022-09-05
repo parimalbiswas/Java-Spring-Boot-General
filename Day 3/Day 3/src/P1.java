8cfc88ec67758cf9d7f6764315e9f1db4cef32dc
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-05 20:49:50 +0530

    .

--- Day 3/Day 3/src/P1.java
+++ Day 3/Day 3/src/P1.java
@@ -1,7 +1,17 @@
-public class P1 {
-    public static void main(String[] args) {
-        A a=new A();
-    }
+var counter=0;
+var count=0;
+for(var i=0;i<arr.length;i++)
+{
+count=1;
+for(var j=i+1;j<arr.length;j++)
+{
+if(arr[i]==arr[j])
+{
+count++;
 }
-
-
+}
+if (count % 2 == 0)
+{
+counter++;
+}
+}
