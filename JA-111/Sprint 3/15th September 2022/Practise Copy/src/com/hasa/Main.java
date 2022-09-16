3ab2e8f55dc5d85a125551f2bad49946e7176f8a
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-09-16 14:28:04 +0530

    .

--- JA-111/Sprint 3/15th September 2022/Practise Copy/src/com/hasa/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hasa;
-
-public class Main {
-    public static void main(String[] args) {
-        Address address = new Address("Howrah","WB");
-        System.out.println(address);
-    }
-}
--- JA-111/Sprint 3/15th September 2022/Practise Copy/src/com/hasa/Person.java
+++
@@ -1,9 +0,0 @@
-package com.hasa;
-
-public class Person {
-
-    String name;
-    Address address;
-
-
-}
