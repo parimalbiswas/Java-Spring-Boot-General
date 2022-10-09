d6e78c998c8196b64b75d046dcf30d06551f02ec
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-10-10 00:49:52 +0530

    .

---
+++ Java SB_SQL_JDBC U5/Sprint1/Day2/Day2_Live/src/_1Demo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class _1Demo {
+    public static void main(String[] args) {
+
+        //------------------DOUBT -----------------
+
+        int arr[]= {10,2,15,12,5};
+        Arrays.sort(arr);
+        //Binaery Search work only sorted Array
+        int index =  Arrays.binarySearch(arr,5);
+        System.out.println(index);
+
+
+        //--------------------DOUBT -----------------
+
+    }
+}
---
+++ Java SB_SQL_JDBC U5/Sprint1/Day2/Day2_Live/src/_2Demo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _2Demo {
+    public static void main(String[] args) {
+        List<String> city = new ArrayList<>();
+        city.add("Delhi");
+        city.add("Kolkata");
+        city.add("Chennai");
+        city.add("Mumbai");
+
+        //SortCurt Way to create List
+        List<String> city1 = Arrays.asList("Delhi","Kolkata","Chennai","Mumbai");
+
+
+        20:54
+
+    }
+}
