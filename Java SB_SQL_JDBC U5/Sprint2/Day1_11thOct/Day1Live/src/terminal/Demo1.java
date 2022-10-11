35f0a7b2277223748c0087994d659a0652a81043
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-10-11 18:25:49 +0530

    .

---
+++ Java SB_SQL_JDBC U5/Sprint2/Day1_11thOct/Day1Live/src/terminal/Demo1.java
@@ -0,0 +1,48 @@
+
+/*
+ * -----------------Terminal Methods----------------
+ * forEach()
+ * collect
+ * min
+ * max
+ * count
+ * get
+ * anyMatch
+ * AllMatch
+ * 
+ * 
+ * -----------------Intermidiate Methods----------------
+ * map()
+ * filter
+ * 
+ * 
+ * 
+ * 
+ * */
+
+
+package terminal;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Demo1
+{
+
+	public static void main(String[] args)
+	{
+		List<String> list = Arrays.asList("one","two","three");
+		
+		Stream<String> s1 = list.stream();
+		
+		s1.forEach(s->System.out.println(s.toUpperCase()));
+		
+		
+		
+	}
+
+}
+
+
+ 
