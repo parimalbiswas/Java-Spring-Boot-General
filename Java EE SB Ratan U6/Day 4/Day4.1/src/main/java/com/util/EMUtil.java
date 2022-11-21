98019cbb6d7b24708b1259ce4d1e67412e354489
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-11-22 00:36:24 +0530

    .

---
+++ Java EE SB Ratan U6/Day 4/Day4.1/src/main/java/com/util/EMUtil.java
@@ -0,0 +1,21 @@
+package com.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMUtil
+{
+	private static EntityManagerFactory emf1;
+	static
+	{
+		emf1 = Persistence.createEntityManagerFactory("account-Unit");
+	}
+
+	public static EntityManager proviEntityManager()
+	{
+		EntityManager em1 = emf1.createEntityManager();
+
+		return em1;
+	}
+}
